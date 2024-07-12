161782cdfb082aecbeeb8dd6b2ff78a6ab3a6205
Thiago Torres <thiago.torres@outlook.com.br>
2024-07-12 13:06:03 -0300

    Performed general test after return device information

---
+++ device_info.py
@@ -0,0 +1,8 @@
+ios_xe = {
+            "address": "sandbox-iosxe-recomm-1.cisco.com",
+            "netconf_port": 830,
+            "restconf_port": 443,
+            "ssh_port": 22,
+            "username": "admin",
+            "password": "C1sco12345"
+          }
