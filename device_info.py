c166b74cca9b619c1511c0be0fe0061946fab273
Thiago Torres <thiago.torres@outlook.com.br>
2024-08-07 11:30:52 -0300

    add 2 nexus

--- device_info.py
+++ device_info.py
@@ -6,3 +6,25 @@ ios_xe = {
             "username": "admin",
             "password": "C1sco12345"
           }
+
+nexos_1 = {
+            "address": "sandbox-nxos-1.cisco.com",
+            "netconf_port": 830,
+            "restconf_port": 443,
+            "ssh_port": 22,
+            "nxapi_port_80": 80,            
+            "nxapi_port_80": 443,
+            "username": "admin",
+            "password": "Admin_1234!"
+          }
+
+nexos_2 = {
+            "address": "sbx-nxos-mgmt.cisco.com",
+            "netconf_port": 830,
+            "restconf_port": 443,
+            "ssh_port": 22,
+            "nxapi_port_80": 80,            
+            "nxapi_port_80": 443,
+            "username": "admin",
+            "password": "Admin_1234!"
+          }
