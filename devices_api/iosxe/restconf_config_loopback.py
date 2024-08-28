0f1eadecf7fe7a409cf05fe04d0052a0cf6c52ae
Thiago Torres <thiago.torres@outlook.com.br>
2024-08-28 13:42:10 -0300

    Adjust restconf initial programs

--- devices_api/iosxe/restconf_config_loopback.py
+++ devices_api/iosxe/restconf_config_loopback.py
@@ -1,8 +1,4 @@
-import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from device_info import ios_xe_sandbox
 import json
 import requests
 from requests.auth import HTTPBasicAuth
@@ -10,24 +6,19 @@ from requests.auth import HTTPBasicAuth
 def main():
     print('--------------------(1)-----------------------')
 
-    config_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'loopback_config.json'))
-
-    with open(config_file_path, 'r') as file:
+    with open("restconf_loopback.json", 'r') as file:
         config_data = json.load(file)
-
-    config_data['Cisco-IOS-XE-native:Loopback']['description'] = "Configured via Restconf - Thiago Torres - CICD Pipeline"
 
     print(config_data)
     print('--------------------(2)-----------------------')
-
  
-    url = f"https://{ios_xe_sandbox['address']}:{ios_xe_sandbox['restconf_port']}/restconf/data"
+    url = f'https://{os.getenv("IOS_XE_ADDRESS")}:443/restconf/data'
     loopback_url = f"{url}/Cisco-IOS-XE-native:native/interface/Loopback"
     headers = {
         "Accept": "application/yang-data+json",
         "Content-Type": "application/yang-data+json"
     }
-    auth = HTTPBasicAuth(ios_xe_sandbox['username'], ios_xe_sandbox['password'])
+    auth = HTTPBasicAuth(os.getenv("IOS_XE_USERNAME"), os.getenv("IOS_XE_PASSWORD"))
 
     response = requests.patch(loopback_url, json=config_data, headers=headers, auth=auth, verify=False)
 
