6f5f117003cc84e55d949737e17af19ad82e2e46
Thiago Torres <thiago.torres@outlook.com.br>
2024-08-28 13:24:06 -0300

    adjust netmiko initial programs

---
+++ devices_api/iosxe/netmiko_config_loopback.py
@@ -0,0 +1,27 @@
+import os
+from netmiko import ConnectHandler
+
+def main():
+    device_params = {
+        'device_type': 'cisco_ios',
+        'host': os.getenv("IOS_XE_ADDRESS"),
+        'username': os.getenv("IOS_XE_USERNAME"),
+        'password': os.getenv("IOS_XE_PASSWORD"),
+        'port': 22,
+        'secret': '',
+        'verbose': False
+    }
+
+    print(device_params)
+
+    connection = ConnectHandler(**device_params)
+
+    with ConnectHandler(**device_params) as connection:
+        with open("netmiko_loopback.txt") as file:
+            config_data = file.read().splitlines()
+
+        config_output = connection.send_config_set(config_data)
+        print("Configuration Output:\n", config_output)
+
+if __name__ == "__main__":
+    main()
